34c3b119b9974e3700ca34e52e5d72f2ed7cb100
lamin5 <say8913@gmail.com>
2025-05-28 11:40:14 +0900

    폴더 이름 수정

--- src/main/java/com/t8/backend/t8/backend/repository/CategoryRepository.java
+++ src/main/java/com/t8/backend/t8/backend/repository/CategoryRepository.java
@@ -1,6 +1,6 @@
 package com.t8.backend.t8.backend.repository;
 
-import com.t8.backend.t8.backend.domain.Category;
+import com.t8.backend.t8.backend.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
