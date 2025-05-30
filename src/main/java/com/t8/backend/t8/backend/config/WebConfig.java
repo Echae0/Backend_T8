22fa237f1145232f6dacefb3bdf3fc58f261f3d3
Echae0 <edujcm01@gmail.com>
2025-05-30 16:28:07 +0900

    WebConfig update

--- src/main/java/com/t8/backend/t8/backend/config/WebConfig.java
+++ src/main/java/com/t8/backend/t8/backend/config/WebConfig.java
@@ -1,16 +1,19 @@
 package com.t8.backend.t8.backend.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/assets/**")
-                .addResourceLocations("classpath:/static/assets/");
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173") // 리액트 dev 서버 주소
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
