cba63d1ca08b75a2f48402f9bcba70e8b1d064e4
lamin5 <say8913@gmail.com>
2025-06-08 11:36:33 +0900

    requestdetail 추가 삭제

--- src/main/java/com/t8/backend/t8/backend/repository/RequestDetailRepository.java
+++
@@ -1,7 +0,0 @@
-// RequestDetailRepository.java
-package com.t8.backend.t8.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RequestDetailRepository extends JpaRepository<RequestDetail, Long> {
-}
